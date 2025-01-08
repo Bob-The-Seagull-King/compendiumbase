c095ecbb5825b370eadeb48f1febd32dd0fc06b4
Bob-The-Seagull-King <laneedwardsbrown@Gmail.com>
2025-01-08 17:27:25 +1300

    first commit

---
+++ src/display/superroutes/CompendiumRoute.tsx
@@ -0,0 +1,40 @@
+import 'bootstrap/dist/css/bootstrap.css'
+import '../../resources/styles/_mainstylesource.scss'
+import React from 'react'
+
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { ROUTES } from '../../resources/routes-constants'
+
+// Components
+import BaseDisplayCompendium from '../pages/BaseDisplayCompendium'
+
+// Classes
+import { ControllerController } from '../../classes/_high_level_controllers/ControllerController'
+import { useGlobalState } from './../../utility/globalstate'
+
+interface IControllerProp {
+    controller : ControllerController; // The controller being passed through
+}
+
+const CompendiumRoute: React.FC<IControllerProp> = (prop) => {
+
+    // State
+    const [theme, setTheme] = useGlobalState('theme');
+
+    // Default to the light theme
+    if ((theme == "" ) || (theme == null)) {
+        setTheme('light');
+    }
+
+    // Return result -----------------------------
+    return (
+        <div className="backgroundBaseColour" data-theme={theme}>
+        <Routes>
+            <Route path={ROUTES.COMPENDIUM_GLOSSARY_ROUTE} element={<BaseDisplayCompendium controller={prop.controller.GlossaryCollectionController}/>} />
+        </Routes>
+        </div>
+    )
+    // -------------------------------------------
+}
+
+export default CompendiumRoute
