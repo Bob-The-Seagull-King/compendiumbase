2ff9f02a8e28288222900086143b11e4e48624e4
Bob-The-Seagull-King <laneedwardsbrown@Gmail.com>
2025-01-16 16:11:01 +1300

    add event runner to globalstore

--- src/utility/globalstate.ts
+++ src/utility/globalstate.ts
@@ -1,11 +1,16 @@
+import { EventRunner } from '../classes/contextevent/contexteventhandler';
 import React from 'react';
 import { createGlobalState } from 'react-hooks-global-state';
 
 const initialState = { theme: InitTheme(),
-                       language: InitLanguage()
+                       language: InitLanguage(),
+                       eventrunner : InitEventHandler()
                     };
 const { useGlobalState } = createGlobalState(initialState);
 
+function InitEventHandler() {
+    return new EventRunner();
+}
 
 function InitLanguage() {
     const theme = localStorage.getItem('language');
